3d1bbaef2bb9403238208f86881d6b17a1103d53
Min RK <benjaminrk@gmail.com>
2023-02-03 10:44:48 +0100

    add projects in a more programmatic way
    show how collab access can be added for any number of accounts

--- collaborative/jupyterhub_config.py
+++ collaborative/jupyterhub_config.py
@@ -17,8 +17,6 @@ c.Authenticator.delete_invalid_users = True
 c.JupyterHub.log_level = 10
 c.JupyterHub.cleanup_servers = True
 
-collab_user = 'project-collab'
-
 c.JupyterHub.load_roles = [
     {
         "name": "admin-servers",
@@ -29,18 +27,6 @@ c.JupyterHub.load_roles = [
         ],
         "groups": ["server-admins"],
     },
-    {
-        "name": "collab-access",
-        "scopes": [
-            "admin-ui",
-            f"admin:servers!user={collab_user}",
-            f"list:users!user={collab_user}",
-            f"access:servers!user={collab_user}",
-        ],
-        "groups": ["project"],
-    },
-
-
     # grant specific users access to all servers
     {
         "name": "full-access",
@@ -52,28 +38,51 @@ c.JupyterHub.load_roles = [
 ]
 
 c.JupyterHub.load_groups = {
-    # project group: access to _this project's_ collaborative server
-    "project": [
-        "minrk",
-        "rcthomas",
-        "shreyas",
-    ],
     # full server admins (doesn't include access)
     "server-admins": [
         "minrk-admin",
     ],
-    "collaborative": [
-        collab_user,
-    ]
+    # collaborative accounts get added to this group
+    "collaborative": [],
 }
+
+projects = {
+    "project": [
+        "minrk",
+        "rcthomas",
+        "shreyas",
+        "mhenders",
+        "colleague",
+    ],
+}
+
+for project_name, members in projects.items():
+    print(f"Adding project {project_name} with members {members}")
+    c.JupyterHub.load_groups[project_name] = members
+    collab_user = f"{project_name}-collab"
+    c.JupyterHub.load_groups["collaborative"].append(collab_user)
+    c.JupyterHub.load_roles.append(
+        {
+            "name": f"collab-access-{project_name}",
+            "scopes": [
+                "admin-ui",
+                f"admin:servers!user={collab_user}",
+                f"list:users!user={collab_user}",
+                f"access:servers!user={collab_user}",
+            ],
+            "groups": [project_name],
+        }
+    )
 
 
 c.JupyterHub.bind_url = "http://127.0.0.1:8000/rtc/"
+
 
 def pre_spawn_hook(spawner):
     group_names = {group.name for group in spawner.user.groups}
     if "collaborative" in group_names:
         spawner.log.info(f"Enabling RTC for user {spawner.user.name}")
         spawner.args.append("--LabApp.collaborative=True")
+
 
 c.Spawner.pre_spawn_hook = pre_spawn_hook
